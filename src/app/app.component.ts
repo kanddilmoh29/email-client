d23a6df6d40f36db22e2107c0067afa8610cce8f
Faris <kanddilmoh@yahoo.com>
2020-09-12 14:35:12 -0400

    create components

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  signedin$: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
+  }
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(() => {
+
+    });
+  }
+
+}
